f4fc41c05cebff5a3f3d4fe5e5031a239ab50563
Paul Abakada <33938655+paulynson@users.noreply.github.com>
2021-04-02 00:07:08 +0100

    h

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+const bar = document.querySelector(".fa-ellipsis-v");
+const popUp = document.querySelector(".pop");
+
+const send = document.querySelector(".fa-send");
+// const msg = document.querySelector(".message").textContent;
+const input = document.querySelector(".input-msg");
+
+bar.addEventListener("click", ()=>{
+		if (popUp.classList.contains("active")) {
+					popUp.classList.remove("active");			
+								
+		}else{
+			popUp.classList.add("active");				
+		}
+			
+})
+
+
+// Send Section
+
+send.addEventListener("click", ()=>{
+	
+	const msgContent = input.value;
+	const msg = document.querySelector(".message").textContent = msgContent;
+})
